d268e76d4c824693cfacb6f5b88f143a97f94ae3
samarpan2001 <sahasamarpan01@gmail.com>
2021-04-14 23:20:28 +0530

    Main dashboard converted to gridview with mainadapter and mainItems model.

--- app/src/main/java/com/example/livemart/MainDashboard.java
+++ app/src/main/java/com/example/livemart/MainDashboard.java
@@ -1,5 +1,6 @@
 package com.example.livemart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -9,70 +10,89 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainDashboard extends AppCompatActivity {
 
+    MainAdapter customAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_dashboard);
-        handleIntent(getIntent());
+        List<MainItems> itemsList = new ArrayList<>();
+        itemsList.add(new MainItems(R.drawable.ic_baseline_search_24, "vegetables"));
+        itemsList.add(new MainItems(R.drawable.ic_baseline_search_24, "fruits"));
+        itemsList.add(new MainItems(R.drawable.ic_baseline_search_24, "apple"));
+        itemsList.add(new MainItems(R.drawable.ic_baseline_search_24, "apple"));
+        itemsList.add(new MainItems(R.drawable.ic_baseline_search_24, "apple"));
+        itemsList.add(new MainItems(R.drawable.ic_baseline_search_24, "apple"));
 
-        //Navigate to vegetables page
-        CardView veg=(CardView) findViewById(R.id.Vegetables);
-        veg.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                Intent i=new Intent(MainDashboard.this,VegDashboard.class);
-                startActivity(i);
-            }
-        });
 
-        //Navigate to fruits page
-        CardView fruits=(CardView) findViewById(R.id.Fruits);
-        fruits.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                Intent i=new Intent(MainDashboard.this,FruitsDashboard.class);
-                startActivity(i);
+
+        GridView gridView = findViewById(R.id.maingrid_view);
+        customAdapter = new MainAdapter(this, R.layout.custom_maindashboard, itemsList);
+        gridView.setAdapter(customAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                MainItems item=customAdapter.items_list_filtered.get(position);
+                String query=item.getTitle().toLowerCase();
+                Intent intent;
+                if(query=="vegetables")
+                {
+                    intent=new Intent(getApplicationContext(),VegDashboard.class);
+                    startActivity(intent);
+                }
+                else if(query=="fruits")
+                {
+                    intent=new Intent(getApplicationContext(),FruitsDashboard.class);
+                    startActivity(intent);
+                }
+
             }
         });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main_dashboard, menu);
 
-//        Associate searchable configuration with the SearchView
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView =
                 (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                customAdapter.getFilter().filter(newText);
+                return true;
+            }
+        });
 
         return true;
     }
 
     @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            // Do work using string
-
-            System.out.println(query);
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.search)
+        {
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
+
 }
